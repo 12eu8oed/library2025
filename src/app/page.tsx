7a8f12a2b19ef4390837e850d698530fd4e72808
승호 <tmdgh9794@naver.com>
2024-12-08 15:35:11 +0900

    Leaflet any type --> String

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,11 @@ async function initializeLeaflet() {
   if (typeof window !== 'undefined' && !leaflet) {
     const L = await import('leaflet')
     leaflet = L
-    delete (L.Icon.Default.prototype as any)._getIconUrl
+    // any 타입을 명시적인 타입으로 변경
+    const icon = L.Icon.Default.prototype as {
+      _getIconUrl?: () => string;
+    };
+    delete icon._getIconUrl;
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
       iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
